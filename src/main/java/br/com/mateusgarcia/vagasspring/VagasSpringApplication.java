c3edc7a97dfde810e6492db0add73f639e8a8e00
mateussk83 <mateussk83@gmail.com>
2023-11-12 11:57:35 -0300

    create project

---
+++ src/main/java/br/com/mateusgarcia/vagasspring/VagasSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.mateusgarcia.vagasspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagasSpringApplication.class, args);
+	}
+
+}
