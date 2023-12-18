ab57307eeb202de1867cd01acd3a73844204386c
mateussk83 <mateussk83@gmail.com>
2023-12-18 08:38:43 -0300

    feat: save in db password encoded

--- src/main/java/br/com/mateusgarcia/vagas_spring/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/mateusgarcia/vagas_spring/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import br.com.mateusgarcia.vagas_spring.exceptions.UserFoundException;
 import br.com.mateusgarcia.vagas_spring.modules.company.entities.CompanyEntity;
 import br.com.mateusgarcia.vagas_spring.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,13 +13,21 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity) {
-        this.companyRepository.findByUsernameOrEmail(
+        this.companyRepository
+                .findByUsernameOrEmail(
                 companyEntity.getUsername(),
                 companyEntity.getEmail()
         ).ifPresent((user) -> {
             throw new UserFoundException();
         });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
+
         return this.companyRepository.save(companyEntity);
     }
 }
