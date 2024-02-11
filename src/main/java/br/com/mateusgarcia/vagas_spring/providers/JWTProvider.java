ec11e3ac64860f475c1528d893da0e89bb5618ff
mateussk83 <mateussk83@gmail.com>
2024-02-11 08:03:01 -0300

    feat: adding swagger documentation in project

--- src/main/java/br/com/mateusgarcia/vagas_spring/providers/JWTProvider.java
+++ src/main/java/br/com/mateusgarcia/vagas_spring/providers/JWTProvider.java
@@ -3,6 +3,7 @@ package br.com.mateusgarcia.vagas_spring.providers;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,21 +13,20 @@ public class JWTProvider {
     @Value("${security.token.secret}")
     private String secretKey;
 
-    public String validateToken(String token) {
+    public DecodedJWT validateToken(String token) {
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
         token = token.replace("Bearer ", "");
 
         try {
-            var subject = JWT.require(algorithm)
+            var tokenDecoded = JWT.require(algorithm)
                     .build()
-                    .verify(token)
-                    .getSubject();
-            return subject;
+                    .verify(token);
+            return tokenDecoded;
         } catch (JWTVerificationException e) {
             e.printStackTrace();
-            return "";
+            return null;
         }
     }
 }
