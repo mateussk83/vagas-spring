6ab46df247e0ce793d95504ff1c0f6a0ca58c238
mateussk83 <mateussk83@gmail.com>
2024-02-13 15:19:41 -0300

    feat: create database test

--- src/test/java/br/com/mateusgarcia/vagas_spring/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/mateusgarcia/vagas_spring/modules/company/controllers/CreateJobControllerTest.java
@@ -6,6 +6,8 @@ package br.com.mateusgarcia.vagas_spring.modules.company.controllers;
 import java.util.UUID;
 
 import br.com.mateusgarcia.vagas_spring.modules.company.dto.CreateJobDTO;
+import br.com.mateusgarcia.vagas_spring.modules.company.entities.CompanyEntity;
+import br.com.mateusgarcia.vagas_spring.modules.company.repositories.CompanyRepository;
 import br.com.mateusgarcia.vagas_spring.utils.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -24,12 +27,16 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
 
     private MockMvc mvc;
 
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @Before
     public void setup(){
@@ -42,6 +49,16 @@ public class CreateJobControllerTest {
     @Test
     public void shouldBeAbleToCreateANewJob() throws Exception{
 
+        var company = CompanyEntity.builder()
+                .description("COMPANY_DESCRIPTION")
+                .email("test@email.com")
+                .password("1234567890")
+                .username("COMPANY_USERNAME")
+                .name("COMPANY_NAME")
+                .build();
+
+        company = companyRepository.saveAndFlush(company);
+
         var createdJobDTO = CreateJobDTO.builder()
                 .benefits("BENEFITS_TEST")
                 .description("DESCRIPTION_TEST")
@@ -51,7 +68,7 @@ public class CreateJobControllerTest {
         mvc.perform(MockMvcRequestBuilders.post("/company/job/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(TestUtils.objectToJson(createdJobDTO))
-                        .header("Authorization", TestUtils.generateToken(UUID.fromString("8fbeed8f-6188-4d72-bd64-28540fc0cc56"), "jAVAGAS_@123#")))
+                        .header("Authorization", TestUtils.generateToken(company.getId(), "jAVAGAS_@123#")))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
     }
--- src/test/java/br/com/mateusgarcia/vagas_spring/utils/TestUtils.java
+++ src/test/java/br/com/mateusgarcia/vagas_spring/utils/TestUtils.java
@@ -32,4 +32,6 @@ public class TestUtils {
                 .sign(algorithm);
         return token;
     }
+
+
 }
